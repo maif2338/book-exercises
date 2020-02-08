afb8c1896c4088e22b182ff1cff658f33d81e47e
Fanny Mai <fannymai@Fannys-MBP-2.lan>
2020-02-07 20:48:45 -0800

    week 5 plus 14 redo

--- chapter-14-exercises/exercise-1/exercise.R
+++ chapter-14-exercises/exercise-1/exercise.R
@@ -1,5 +1,6 @@
 # Exercise 1: reading and querying a web API
-
+#install.packages("httr")
+library("httr") 
 # Load the httr and jsonlite libraries for accessing data
 # You can also load `dplyr` if you wish to use it
 
@@ -7,24 +8,27 @@
 # Create a variable base_uri that stores the base URI (as a string) for the 
 # Github API (https://api.github.com)
 
-
+base_uri <- "https://api.github.com"
 # Under the "Repositories" category of the API documentation, find the endpoint 
 # that will list _repos in an organization_. Then create a variable named
 # `org_resource` that stores the endpoint for the `programming-for-data-science`
 # organization repos (this is the _path_ to the resource of interest).
 
-
+org_resource <- "/programming-for-data-science"
 # Send a GET request to this endpoint (the `base_uri` followed by the 
 # `org_resource` path). Print the response to show that your request worked. 
 # (The listed URI will also allow you to inspect the JSON in the browser easily).
-
+response <- GET(base_uri, org_resource)
+print(response)
 
 # Extract the content of the response using the `content()` function, saving it
 # in a variable.
 
-
+response_text <- content(response)
 # Convert the content variable from a JSON string into a data frame.
-
+{
+  "org_resource": "/programming-for-data-science",
+}
 
 # How many (public) repositories does the organization have?
 
@@ -52,4 +56,3 @@
 
 
 # What are the full names of the top 5 repos in the search results?
-
